7d46572e609460f97601e86fdd8c7bc7692a87f4
Waguti Warren <warrenw639@gmail.com>
2023-04-19 00:09:57 +0300

    trying to link other signup forms

--- routes/aoRoute.js
+++ routes/aoRoute.js
@@ -1,14 +1,40 @@
 const express = require('express');
 const router = express.Router();
 const User = require("../models/userModel");
-const connectEnsureLogin = require("connect-ensure-login");
+// const connectEnsureLogin = require("connect-ensure-login");
 
 
 //connectEnsureLogin.ensureLoggedIn(), put back later
 router.get("/aodash",  (req, res)=>{
     res.render("aoDash");
-})
+});
 
+
+router.get("/fOneRegister",  (req, res)=>{
+    res.render("registerFO");
+})
+router.post("/fOneRegister", async (req, res) => {
+    console.log(req.body);
+    try {
+        const user = new User(req.body);
+        let uniqueid = await User.findOne({ uniqueid: req.body.uniqueid });
+        if (uniqueid) {
+            return res.send("This unique id already exists");
+        }
+        else {
+             User.register(user, req.body.password, (error) => {
+                if (error) {
+                    throw error
+                };
+                res.redirect("login");
+            })
+        }
+    }
+    catch(error){
+        res.status(400).send("sorry it seems there is trouble accessing this page");
+        console.log(error);     
+    }
+})
 
 
 module.exports = router;
--- routes/auth/fORoute.js
+++ routes/auth/fORoute.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const fOneSchema = require("../../models/registerFoModel");
+const connectEnsureLogin = require("connect-ensure-login");
+// const User = require("../models/userModel");  
 
 router.get("/fOneRegister", (req, res) => {
     res.render("registerFO")
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 router.get("/fOneslist", async(req, res) =>{
     try{
         let items = await fOneSchema.find();
